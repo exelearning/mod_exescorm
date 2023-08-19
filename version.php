0de186499ebd5d7e7927583e2abe9edd35d2ed7b
ignaciogros <ignacio@gros.es>
2023-08-19 16:33:59 +0200

    Plugin version.

--- version.php
+++ version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2023061400;    // The current module version (Date: YYYYMMDDXX).
+$plugin->version = 2023073000;    // The current module version (Date: YYYYMMDDXX).
 $plugin->requires = 2020061500;    // Moodle 3.9 to 4.1.
 $plugin->component = 'mod_exescorm';   // Full name of the plugin (used for diagnostics).
